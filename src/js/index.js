1d211f52c464970be3028243693a901337a84ba5
Teo <kesyalox@gmaile.com>
2024-10-15 22:28:07 +0300

    Add mouse movement to the share  gallery

--- src/js/index.js
+++ src/js/index.js
@@ -34,8 +34,57 @@ initializeBlogSlider();
 import { initializeRoomsSlider } from "./modules/utils/swiperSlider.js";
 initializeRoomsSlider();
 
-import shareScrollBar from "./modules/share-scrollbar.js";
-document.addEventListener("DOMContentLoaded", shareScrollBar);
-
 AOS.init();
 Fancybox.bind('[data-fancybox="share-gallery"]', {});
+
+import isMobile from "./modules/utils/isMobile.js";
+
+const shareGallery = document.querySelector(".share__body");
+
+if (shareGallery && !isMobile()) {
+    const shareGallery = document.querySelector(".share__body");
+    const shareItems = document.querySelector(".share__items");
+    const shareColumns = document.querySelectorAll(".share__column");
+
+    const speed = parseFloat(shareGallery.dataset.speed) || 1;
+
+    let positionX = 0;
+    let coordXprocent = 0;
+
+    function setMouseGalleryStyle() {
+        let ShareItemsWidth = 0;
+        shareColumns.forEach((column) => {
+            ShareItemsWidth += column.offsetWidth;
+        });
+
+        const shareDiff = ShareItemsWidth - shareGallery.offsetWidth;
+        const distX = coordXprocent - positionX;
+
+        positionX += distX * speed;
+
+        // Убираем ограничения, чтобы позволить двигаться в обе стороны
+        // Применяем только ограничение по ширине колонок
+        positionX = Math.max(-shareDiff, Math.min(positionX, shareDiff));
+
+        const position = (shareDiff / 200) * positionX; // инвертируем позицию для корректного смещения
+        shareItems.style.transform = `translate3d(${-position}px, 0, 0)`;
+
+        // Продолжаем анимацию, если есть движение
+        if (Math.abs(distX) > 0.1) {
+            requestAnimationFrame(setMouseGalleryStyle);
+        } else {
+            shareGallery.classList.remove("_init");
+        }
+    }
+
+    shareGallery.addEventListener("mousemove", function(e) {
+        const shareWidth = shareGallery.offsetWidth;
+        const coordX = e.pageX - shareWidth / 2;
+        coordXprocent = (coordX / shareWidth) * 200;
+
+        if (!shareGallery.classList.contains("_init")) {
+            requestAnimationFrame(setMouseGalleryStyle);
+            shareGallery.classList.add("_init");
+        }
+    });
+}
--- src/js/modules/share-scrollbar.js
+++
@@ -1,7 +0,0 @@
-function shareScrollBar() {
-    const scrollContainer = document.querySelector(".share__body");
-
-    scrollContainer.scrollLeft = 300;
-}
-
-export default shareScrollBar;
